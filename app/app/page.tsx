a3dfbd6607fb2d53a616dbfb6f95612c4340aa42
ZechNg <tiamyang@gmail.com>
2024-09-22 03:13:46 +0800

    change to white background theme

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -97,7 +97,7 @@ function AppContent() {
   };
 
   return (
-    <div className="h-screen bg-gray-900 text-white flex flex-col font-sans">
+    <div className="h-screen bg-white-900 text-black flex flex-col font-sans">
       <header className="absolute top-0 left-0 w-full p-4 flex justify-between items-center">
         <div
           className="text-xl font-bold cursor-pointer"
