7421f31e51c6bc8bf41febad3dcf3f5f1a13d9e5
taijusanagi <taijusanagi@gmail.com>
2024-09-22 03:28:36 +0800

    recover metamask

--- app/app/providers.tsx
+++ app/app/providers.tsx
@@ -1,10 +1,14 @@
-import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import {
+  DynamicContextProvider,
+  EthereumWalletConnectors,
+} from "../lib/dynamic";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <DynamicContextProvider
       settings={{
         environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENV_ID || "",
+        walletConnectors: [EthereumWalletConnectors],
       }}
     >
       {children}
