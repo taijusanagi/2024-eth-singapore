5340a4979a6026a979dae21da68c737eab76548f
ZechNg <tiamyang@gmail.com>
2024-09-21 14:43:44 +0800

    load bg

--- app/app/PhaserGame.tsx
+++ app/app/PhaserGame.tsx
@@ -10,8 +10,12 @@ const PhaserGame = () => {
             import('phaser').then(Phaser => {
                 const config = {
                     type: Phaser.AUTO,
-                    width: 800,
-                    height: 600,
+                    scale: {
+                        mode: Phaser.Scale.RESIZE, // Automatically resize the game to fit the window
+                        autoCenter: Phaser.Scale.CENTER_BOTH, // Center the game in both directions
+                        width: window.innerWidth,
+                        height: '100%',
+                    },
                     parent: gameRef.current,
                     scene: {
                         preload: preload,
@@ -34,13 +38,17 @@ const PhaserGame = () => {
         };
     }, []);
 
-    const preload = () => {
+    function preload(this: Phaser.Scene) {
         // Load assets here
+        this.load.image('bg_main', 'phaser/bg1.png');
     };
 
     function create(this: Phaser.Scene) {
         // Use 'this' which now refers to Phaser.Scene, so TypeScript understands it's not undefined
         this.add.text(100, 100, 'Hello Phaser in React', { color: '#fff' });
+
+        var bg_layer = this.add.container();
+        bg_layer.add(this.add.image(0, 0, 'bg_main').setOrigin(0, 0));
     }
 
     const update = () => {
@@ -50,7 +58,7 @@ const PhaserGame = () => {
     return (
         <div
             ref={gameRef}
-            style={{ width: '800px', height: '600px' }} // Adjust based on your game size
+            style={{ width: '100%', height: '100vh' }} // Adjust based on your game size
         />
     );
 };
