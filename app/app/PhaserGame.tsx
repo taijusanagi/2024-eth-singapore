3b5b6d0ca2bd0905bcde7a5131f3895a2ae181c9
ZechNg <tiamyang@gmail.com>
2024-09-21 21:13:01 +0800

    add opponent

--- app/app/PhaserGame.tsx
+++ app/app/PhaserGame.tsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useRef } from 'react';
 
+enum GAME_STATES {
+    IDLE = "idle",
+    COMBAT = "combat",
+    HARVESTING = "harvesting",
+};
+
 const PhaserGame = () => {
     const gameRef: any = useRef(null); // To keep reference to the game DOM element
     const gameInstanceRef: any = useRef(null); // To store the Phaser game instance
+    let gameState: GAME_STATES = GAME_STATES.IDLE;
     let player: any;
-    let glasses: any;
+    let opponent: any;
     let attackTween: any;
     let bg_foreground: any;
     let bg_mid: any;
     let bg_far: any;
     let bg_container: any;
     let player_Y: any;
+    let smoke_sprite: any;
+    let player_X: any;
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -46,33 +55,34 @@ const PhaserGame = () => {
         };
     }, []);
 
+    function preload_fx() {
+        const activeScene = gameInstanceRef.current.scene.scenes[0];
+        activeScene.load.spritesheet('hitEffect', 'phaser/fx/hit.png', {
+            frameWidth: 133,  // Width of each frame
+            frameHeight: 133  // Height of each frame
+        });
+
+    }
+
     function preload(this: Phaser.Scene) {
         // Load assets here
         this.load.image('bg_mid', 'phaser/bg/city/1.png');
         this.load.image('bg_foreground', 'phaser/bg/city/foreground.png');
         this.load.image('bg_far', 'phaser/bg/city/far-buildings.png');
+        this.load.image('bg_wall', 'phaser/bg/wall.png');
 
-        this.load.spritesheet('hitEffect', 'phaser/fx/hit.png', {
-            frameWidth: 133,  // Width of each frame
-            frameHeight: 133  // Height of each frame
-        });
-
-        this.load.image('glasses1', 'phaser/nouns/1.png');
-        this.load.image('glasses2', 'phaser/nouns/2.png');
-        this.load.image('glasses3', 'phaser/nouns/3.png');
-        this.load.image('glasses4', 'phaser/nouns/4.png');
-        this.load.image('glasses5', 'phaser/nouns/5.png');
-        this.load.image('glasses6', 'phaser/nouns/6.png');
-        this.load.image('glasses7', 'phaser/nouns/7.png');
-        this.load.image('glasses8', 'phaser/nouns/8.png');
-        this.load.image('glasses9', 'phaser/nouns/9.png');
-        this.load.image('glasses10', 'phaser/nouns/10.png');
-        this.load.image('glasses11', 'phaser/nouns/11.png');
+        preload_fx();
 
         this.load.spritesheet('noun1', 'phaser/chars/nouns/1.png', {
             frameWidth: 128,  // Replace with your sprite width
             frameHeight: 128  // Replace with your sprite height
         });
+
+        this.load.spritesheet('noun2', 'phaser/chars/nouns/2.png', {
+            frameWidth: 128,  // Replace with your sprite width
+            frameHeight: 128  // Replace with your sprite height
+        });
+
         this.load.spritesheet('playerRun', 'phaser/chars/1/Run.png', {
             frameWidth: 128,  // Replace with your sprite width
             frameHeight: 128  // Replace with your sprite height
@@ -101,18 +111,21 @@ const PhaserGame = () => {
         bg_container.add(bg_foreground);
         bg_container.setScale(2);
 
-        this.anims.create({
-            key: 'hitFX',  // Name of the animation
-            frames: this.anims.generateFrameNumbers('hitEffect', { start: 0, end: 9 }),  // Use frames 0 to 9
-            frameRate: 30,  // Speed of the animation
-            repeat: 0  // Play once and stop
-        });
+        const bg_wall = this.add.tileSprite(0, 560,
+            this.scale.width * 2, 300, 'bg_wall').setOrigin(0, 0);
+        bg_wall.setScale(0.5);
+
+        create_fx();
 
         const canvasWidth = this.cameras.main.width;
         const canvasHeight = this.cameras.main.height;
+        player_X = canvasWidth / 2;
         player_Y = canvasHeight / 2 + 90;
-        player = this.add.sprite(canvasWidth / 2, player_Y, 'noun1');
+        player = this.add.sprite(player_X, player_Y, 'noun1');
         player.setScale(1);
+
+        opponent = this.add.sprite(canvasWidth / 2 + 150, player_Y, 'noun2');
+        opponent.flipX = true;
 
         // Tween to simulate bouncing or walking up and down
         this.tweens.add({
@@ -129,6 +142,33 @@ const PhaserGame = () => {
                 console.log('Bounce complete');  // Optional: callback when the tween completes
             }
         });
+
+        // Tween to simulate bouncing or walking up and down
+        this.tweens.add({
+            targets: opponent,  // The sprite to animate
+            y: player_Y - 10,  // Target y position to "bounce" up to
+            duration: 280,  // Time to reach the target (in ms)
+            ease: 'Sine.easeInOut',  // Easing function for smooth motion
+            yoyo: true,  // Return to the original y position
+            repeat: -1,  // Repeat forever
+            onYoyo: () => {
+                console.log('Going back down');  // Optional: callback when returning to original position
+            },
+            onComplete: () => {
+                console.log('Bounce complete');  // Optional: callback when the tween completes
+            }
+        });
+    }
+
+    function create_fx() {
+        const activeScene = gameInstanceRef.current.scene.scenes[0];
+        activeScene.anims.create({
+            key: 'hitFX',  // Name of the animation
+            frames: activeScene.anims.generateFrameNumbers('hitEffect', { start: 0, end: 9 }),  // Use frames 0 to 9
+            frameRate: 30,  // Speed of the animation
+            repeat: 0  // Play once and stop
+        });
+
     }
 
     function PlayHitFX(x: number, y: number) {
@@ -148,24 +188,39 @@ const PhaserGame = () => {
     }
 
     function update() {
+        if (gameState !== GAME_STATES.IDLE) {
+            return;
+        }
+
         // Scroll the background by updating the tile position
         if (bg_mid) {
             bg_mid.tilePositionX += 0.2;  // Move background to the left for scrolling effect
         }
+
         if (bg_foreground) {
             bg_foreground.tilePositionX += 0.5;  // Move background to the left for scrolling effect
         }
     }
 
+    function RevealOpponent() {
+        opponent.visible = true;
+    }
+
     function PlayAttack() {
         const activeScene = gameInstanceRef.current.scene.scenes[0];
+
+        if (!player) {
+            console.log('Player sprite not found');
+            return;
+        }
+
+        gameState = GAME_STATES.COMBAT;
 
         PlayHitFX(player.x, player.y);
 
         // Set the range for left and right movement (adjust the values as needed)
-        const initialX = player.x;
-        const leftMove = initialX - 20;  // Move 50 pixels to the left
-        const rightMove = initialX + 50;  // Move 50 pixels to the right
+        const leftMove = player_X - 20;  // Move 50 pixels to the left
+        const rightMove = player_X + 50;  // Move 50 pixels to the right
 
         attackTween = activeScene.tweens.add({
             targets: player,  // The sprite to animate
@@ -184,6 +239,7 @@ const PhaserGame = () => {
                 activeScene.tweens.add({
                     targets: player,
                     x: leftMove,  // Move to the left
+                    angle: 0,
                     duration: 100,
                     ease: 'Sine.easeInOut',
                     yoyo: true,  // Return to the original position
@@ -211,7 +267,7 @@ const PhaserGame = () => {
     }
 
     function PlayerEquip() {
-        glasses.setTexture("glasses" + Math.floor(Math.random() * 11 + 1));
+        //glasses.setTexture("glasses" + Math.floor(Math.random() * 11 + 1));
     }
 
     return (
